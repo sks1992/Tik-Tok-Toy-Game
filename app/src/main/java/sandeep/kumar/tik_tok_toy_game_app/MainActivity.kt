14abfa58cc1b07e36029f40b2435a7d2b380949c
sks1992 <63912583+sks1992@users.noreply.github.com>
2021-06-23 13:43:41 +0530

    create ui

---
+++ app/src/main/java/sandeep/kumar/tik_tok_toy_game_app/MainActivity.kt
@@ -0,0 +1,15 @@
+package sandeep.kumar.tik_tok_toy_game_app
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
